bdc4472ed2cfbb6aa98e3ef95472ef0cd571a0c6
Keshav6918 <keshav_bhardwaj@epam.com>
2025-02-10 18:46:50 +0530

    Update task2.js

--- task2.js
+++ task2.js
@@ -1,1 +1,19 @@
+const tasks = [   
+    { taskName: "Write report", completed: true, priority: 2 },   
+    { taskName: "Attend meeting", completed: false, priority: 3 },   
+    { taskName: "Fix bug", completed: false, priority: 1 },   
+    { taskName: "Update website", completed: true, priority: 4 } 
+];
 
+const FindHPrior = tasks.find(task => task.priority < 3);
+console.log(FindHPrior);
+
+const FindIndNcompl = tasks.findIndex(task => !task.completed);
+console.log(FindIndNcompl);
+
+const completedTaskCount = tasks.reduce((count, task) => task.completed ? count + 1 : count, 0);
+console.log('Number of Completed Tasks:', completedTaskCount);
+
+tasks.forEach(task => {
+    console.log(`${task.taskName}`)
+})
